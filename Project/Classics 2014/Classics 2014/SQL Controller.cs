40f9b647e1bf9cb984aca4f84fb8d32b622ab9fa
Sprackle <jamiemills505@gmail.com>
2014-07-22 13:05:49 +0100

    SQL Controller Changes

--- Project/Classics 2014/Classics 2014/SQL Controller.cs
+++ Project/Classics 2014/Classics 2014/SQL Controller.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
 
 namespace Classics_2014
 {
     class SQL_Controller
     {
+        private string server;
+        private string database;
+        private string user;
+        private MySqlConnection connection;
+        private string error;
+        private Process localDatabaseMain = new Process();
+        private string CurrentDirectory = Directory.GetCurrentDirectory();
+
+        public void MySQL_Controller(string Server, string Database, string User)
+        {
+            this.server = Server;
+            this.database = Database;
+            this.user = User;
+            //SetupConnection();
+			//StartDatabase();
+        }
     }
 }

d1280e74e3bd0a45f1fb00ead01387f2103b2213
Sprackle <jamiemills505@gmail.com>
2015-02-15 13:32:40 +0000

    Laptop Upload
    Ehhhh

---
+++ Project/Classics 2014/Classics 2014/EventLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CMS
+{
+    class EventLoader
+    {
+        LoadScreen loadScreen;
+        public List<Event> events;
+
+        public EventLoader()
+        {
+        }
+    }
+}
---
+++ Project/Classics 2014/Classics 2014/Forms/LoadScreen.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CMS
+{
+    partial class LoadScreen : UserControl
+    {
+        EventLoader EventLoader;
+
+        public LoadScreen(EventLoader EventLoader)
+        {
+            this.EventLoader = EventLoader;
+            InitializeComponent();
+        }
+
+        private void refreshEventGrid()
+        {
+            dataGridEvents.Rows.Clear();
+            foreach (Event e in EventLoader.events)
+            {
+                dataGridEvents.Rows.Add(e.EventID, e.Name, e.Date.ToShortDateString(), e.EventType.ToString());
+            }
+        }
+
+        public int getSelectedEventIndex()
+        {
+            if (dataGridEvents.SelectedRows != null)
+            {
+                if (dataGridEvents.SelectedRows.Count > 0)
+                {
+                    int eventID = Convert.ToInt16(dataGridEvents.SelectedRows[0].Cells[0].Value);
+
+                    for (int i = 0; i < EventLoader.events.Count; i++)
+                    {
+                        if (EventLoader.events[i].EventID == eventID)
+                        {
+                            return i;
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
